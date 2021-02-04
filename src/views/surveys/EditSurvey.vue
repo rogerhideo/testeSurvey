21ad55fa98dbb4cb929554725791d7269ba3ec82
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-04 19:16:31 -0400

    1

--- src/views/surveys/EditSurvey.vue
+++ src/views/surveys/EditSurvey.vue
@@ -8,18 +8,18 @@
       	</div>
 
 		<div class="md:flex-wrap w-4/5 bg-white shadow-md rounded-md border-gray-200 hover:shadow-xl w-min-2xl p-5 mt-5 border-l-3 focus:border-blue-500"> 
-        	<div class="md:flex md:justify-between md:items-center w-full text-left "> 
+        	<div class="sm:flex sm:justify-between items-center w-full text-left "> 
 				<input 
 					type="text" 
 					name="description" 
 					id="description" 
 					placeholder= "Digite a pergunta aqui "
-					class="flex w-2/4 outline-none mt-1  p-1 mb-3  bg-gray-100 border-b-2 border-black    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
+					class="flex w-full sm:w-2/4 outline-none mt-1  p-1 mb-3  bg-gray-100 border-b-2 border-black    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
 				> 
 				<div >
 
 					<span class="text-gray-300"> Tipo de questão </span>
-					<div class="inline-block">
+					<div class="inline-block ">
 						<button 
 							class="ml-1 flex items-center justify-center h-4 w-4 rounded-full ring-2 ring-gray-300 focus:ring-inset  hover:ring-gray-400 focus:outline-none" @click="isOpen = true"
 						>	
