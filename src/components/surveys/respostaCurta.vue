522d42d9a8c21dd77de552c94a99b039fd7e2cb0
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-05 13:18:24 -0400

    1

--- src/components/surveys/respostaCurta.vue
+++ src/components/surveys/respostaCurta.vue
@@ -5,7 +5,7 @@
                 type="text" 
                 name="description" 
                 id="description" 
-                placeholder= "Rsposta aqui"
+                placeholder= "Resposta aqui"
                 class="flex w-full outline-none mt-1  p-1 mb-3  border-b-1 border-gray-300    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
             > 
             
