1d8c006bb08934ab0da7a4a8f2dce76217be4388
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-05 12:58:11 -0400

    1

---
+++ src/components/surveys/respostaCurta.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="w-full">
+            <input 
+                type="text" 
+                name="description" 
+                id="description" 
+                placeholder= "Digite a pergunta aqui "
+                class="flex w-full outline-none mt-1  p-1 mb-3  bg-gray-100 border-b-2 border-black    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
+            > 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+		</div>	
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
