7cca7b0fe43e2309e8a10b973000c47dda219cf9
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-08 17:32:10 -0400

    08-02-2021

--- src/components/surveys/miniSurvey.vue
+++ src/components/surveys/miniSurvey.vue
@@ -9,7 +9,7 @@
 							name="titleQuestion" 
 							id="titleQuestion" 
 							placeholder= "Digite a pergunta aqui "
-                            v-model="realValue[0].titleQuestion"
+                            v-model="value[valueLength].titleQuestion"
 							class="flex w-full outline-none mt-1  p-1 mb-3  bg-gray-100 border-b-2 border-black    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
 						> 
 					</div>	
@@ -27,17 +27,17 @@
 					</div>
 				</div> 
 				<div v-if="isOpenSelected">
-					<checkBox v-show="selected === 'Caixa de Seleção'"/>
-					<listaSuspensa v-show="selected === 'Lista Suspensa'"/>
-					<multiplaEscolha v-show="selected === 'Multiplha Escolha'"/>
-					<respostaLonga v-show="selected === 'Parágrafo'"/>
-					<respostaCurta v-show="selected === 'Resposta Curta'"/>
+					<checkBox v-if="value[valueLength].tipoSurvey === 4"/>
+					<listaSuspensa v-if="value[valueLength].tipoSurvey === 5"/>
+					<multiplaEscolha v-if="value[valueLength].tipoSurvey === 3"/>
+					<respostaLonga v-if="value[valueLength].tipoSurvey === 2"/>
+					<respostaCurta v-if="value[valueLength].tipoSurvey === 1"/>
 				</div>
 			</div>
 			<div class="md:flex-grow-0 bg-white shadow-md rounded-md border-gray-200 hover:shadow-xl w-min-2xl  border-l-3 focus:border-blue-500 p-5 mt-5 ml-5" v-if="isOpenOptions" > 
 					<div class="flex ">				
 							<button 
-								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-none" @click="toggleModalSelected" @click.stop="selected = 'Resposta Curta'" 
+								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-none" @click="toggleModalSelected" @click.stop="value[valueLength].tipoSurvey = 1" 
 							
 							>	
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -51,7 +51,7 @@
 					<div class="flex mt-3">				
 							<button 
 								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-none" @click="toggleModalSelected"
-								@click.stop="selected = 'Parágrafo'" 
+								 @click.stop="value[valueLength].tipoSurvey = 2" 
 							>	
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 									<path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
@@ -63,7 +63,7 @@
 					</div>	
 					<div class="flex mt-3">				
 							<button 
-								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-nonee" @click="toggleModalSelected" @click.stop="selected = 'Multiplha Escolha'"  
+								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-nonee" @click="toggleModalSelected"  @click.stop="value[valueLength].tipoSurvey = 3"
 							>	
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 									<path fill-rule="evenodd" d="M4.649 3.084A1 1 0 015.163 4.4 13.95 13.95 0 004 10c0 1.993.416 3.886 1.164 5.6a1 1 0 01-1.832.8A15.95 15.95 0 012 10c0-2.274.475-4.44 1.332-6.4a1 1 0 011.317-.516zM12.96 7a3 3 0 00-2.342 1.126l-.328.41-.111-.279A2 2 0 008.323 7H8a1 1 0 000 2h.323l.532 1.33-1.035 1.295a1 1 0 01-.781.375H7a1 1 0 100 2h.039a3 3 0 002.342-1.126l.328-.41.111.279A2 2 0 0011.677 14H12a1 1 0 100-2h-.323l-.532-1.33 1.035-1.295A1 1 0 0112.961 9H13a1 1 0 100-2h-.039zm1.874-2.6a1 1 0 011.833-.8A15.95 15.95 0 0118 10c0 2.274-.475 4.44-1.332 6.4a1 1 0 11-1.832-.8A13.949 13.949 0 0016 10c0-1.993-.416-3.886-1.165-5.6z" clip-rule="evenodd" />
@@ -75,7 +75,7 @@
 					</div>	
 					<div class="flex mt-3">				
 							<button 
-								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-none" @click="toggleModalSelected" @click.stop="selected = 'Caixa de Seleção'"  
+								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-none" @click="toggleModalSelected"  @click.stop="value[valueLength].tipoSurvey = 4"
 							>	
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 									<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
@@ -87,7 +87,7 @@
 					</div>	
 					<div class="flex mt-3">				
 							<button 
-								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-nonee" @click="toggleModalSelected" @click.stop="selected = 'Lista Suspensa'" 
+								class="ml-1  mt-1 flex items-center justify-center h-4 w-4 rounded-full   focus:ring-inset  hover:ring-gray-400 outline-nonee" @click="toggleModalSelected"  @click.stop="value[valueLength].tipoSurvey = 5"
 							> 
 								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 									<path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
@@ -144,21 +144,21 @@
 			toggleModalSelected() {
 				this.isOpenSelected = true
 				this.isOpenOptions = false
-                switch (this.selected) {
-                    case 'Caixa de Seleção':
-                        this.realValue[0].tipoSurvey = 5;
+                switch (this.value[this.valueLength].tipoSurvey) {
+                    case 5:
+                        this.selected = 'Caixa de Seleção';
                         break;
-                    case 'Lista Suspensa':
-                        this.realValue[0].tipoSurvey = 4;
+                    case 4:
+                        this.selected = 'Lista Suspensa';
                         break;
-                    case 'Multiplha Escolha':
-                        this.realValue[0].tipoSurvey = 3;
+                    case 3:
+                        this.selected = 'Multiplha Escolha';
                         break;
-                    case 'Parágrafo':
-                        this.realValue[0].tipoSurvey = 2;
+                    case 2:
+                        this.selected = 'Parágrafo';
                         break;
-                    case 'Resposta Curta':
-                        this.realValue[0].tipoSurvey = 1;
+                    case 1:
+                        this.selected = 'Resposta Curta';
                         break;
                     }
 			},
@@ -170,6 +170,9 @@
         computed:{
             realValue() {
                 return this.value ? this.value : this.list;
+            },
+            valueLength(){
+                return this.value.length -1 ;
             }
         }  
     }
