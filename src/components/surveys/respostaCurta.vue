044e359b7f368a567dc5a76c54f6f0ff511e9fd2
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-09 14:08:16 -0400

    09-02-2021

--- src/components/surveys/respostaCurta.vue
+++ src/components/surveys/respostaCurta.vue
@@ -3,31 +3,34 @@
         <div class="flex w-full">
             <input 
                 type="text" 
-                name="description" 
-                id="description" 
-                placeholder= "Resposta aqui"
+                name="option" 
+                id="option" 
+                placeholder= "Opção"
                 v-model= "value.inputPayload"
                 @input= "createNewOption"
                 class="flex w-full outline-none mt-1  p-1 mb-3  border-b-1 border-gray-300    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
             >      
-            <respostaCurta v-model="realValue.options"/>    
+            <respostaCurta v-if="value.options.inputPayload === 1" v-model="value.options"/>    
 		</div>	
         
     </div>
 </template>
 
 <script>
+    import respostaCurta from '@/components/surveys/respostaCurta.vue';
     export default {
-    
+        components:{
+            respostaCurta
+        },
         props: {
             value: {
                 required: false,
-                type: Array,
+                type: Object,
                 default: null
             },
             list: {
                 required: false,
-                type: Array,
+                type: Object,
                 default: null
             }
         },
@@ -37,7 +40,7 @@
             },
             createNewOption(){
                 this.value.options = {
-                    inputPayload: '',
+                    inputPayload: 1,
 					options:{}
                 }
             }
