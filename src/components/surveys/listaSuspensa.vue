5435115b056bddc1775cc725504e3ba6992898c0
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-11 21:48:36 -0400

    10-02-2021

--- src/components/surveys/listaSuspensa.vue
+++ src/components/surveys/listaSuspensa.vue
@@ -8,15 +8,15 @@
                 placeholder= "Opção"
                 v-model= "op.text"
                 class="flex w-full outline-none mt-1  p-1 mb-3  border-b-1 border-gray-300    shadow-sm sm:text-sm  focus:border-blue-500  rounded-md"
-            > 
+            >  
             <button 
-                class="ml-1 flex items-center justify-center h-6 w-6 rounded-full  focus:ring-inset  hover:ring-gray-400 focus:outline-none" @click="toggleModalOptions"
+                class="ml-1 flex items-center justify-center h-6 w-6 rounded-full  focus:ring-inset  hover:ring-gray-400 focus:outline-none" 
             >	
                 <svg class="w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
-            </button>  
-		</div>
+            </button>             
+		</div>	
         <input 
                 type="text" 
                 name="option" 
@@ -30,8 +30,8 @@
 </template>
 
 <script>
-  export default {
-        name: "listaSupensa",
+    export default {
+        name: "listaSuspensa",
         data(){
             return{
                 inputPayload:''
