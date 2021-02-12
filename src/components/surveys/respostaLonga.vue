b7495c85884203c0733e92f982af2ad0d792a467
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-11 21:31:15 -0400

    10-02-2021

--- src/components/surveys/respostaLonga.vue
+++ src/components/surveys/respostaLonga.vue
@@ -3,9 +3,7 @@
         <div class="flex w-full">
             <textarea name="comment" rows="5" placecholder="Enter text here..." v-model= "value.inputPayload" @keyup.enter= "createNewOption" class="flex w-full border-2 outline-none focus:outline-none border-gray-300 "></textarea>
         </div>
-         <div class="flex w-full">
-            <respostaLonga v-model="realValue.options"/> 
-        </div>
+         
     </div>
 
 
@@ -20,29 +18,13 @@
                 type: Object,
                 default: null
             },
-            list: {
-                required: false,
-                type: Object,
-                default: null
-            }
         },
         methods: {
 			emitter(value) {
                  this.$emit("input", value);
-            },
-            createNewOption(){
-                this.value.options = {
-                    inputPayload: '',
-					options:{}
-                }
-            }
-                 
-        },
-        computed:{
-            realValue() {
-                return this.value ? this.value : this.list;
-            }         
-        } 
+            }            
+        }
+        
     }
 </script>
 
