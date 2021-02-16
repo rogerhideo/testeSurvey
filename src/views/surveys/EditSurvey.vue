ed60ea084222ca5c5e8334e6ff71cbfe0f81e947
rogerhideo <roger.hideo@aluno.ufr.edu.br>
2021-02-15 20:57:29 -0400

    15-02-2021

--- src/views/surveys/EditSurvey.vue
+++ src/views/surveys/EditSurvey.vue
@@ -30,9 +30,8 @@
 				<div class="w-8 min-h-full bg-white opacity-20 hover:opacity-70 items-center flex justify-center transition duration-500 ease-in-out hover:shadow-md rounded-full">
 					<i class="fa fa-arrows my-handle">
 						<svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4" />
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4" />
 						</svg>
-
 					</i>
 				</div>
 			<miniSurvey :value="quest"/>
